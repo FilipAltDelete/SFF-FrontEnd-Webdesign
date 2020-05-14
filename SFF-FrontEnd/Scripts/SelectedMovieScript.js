d284d895857f2dc0b12ecb7e7cf6c2e77ff36b03
Filip Larsson <filipaltdelete@AI.local>
2020-05-14 14:30:21 +0200

    Added Trivia Function

--- SFF-FrontEnd/Scripts/SelectedMovieScript.js
+++ SFF-FrontEnd/Scripts/SelectedMovieScript.js
@@ -32,7 +32,36 @@ function buildList(data) {
 
 function showButtons() {
   document.getElementById("Buttons").innerHTML =
-    '<button id="rentButton" class="rentButton">Rent</button><br></br><button id="returnButton" class="returnButton">Return</button><br></br><button id="addTrivia" class="triviaButton">Add Trivia</button>';
+    '<button id="rentButton" class="rentButton">Rent</button><br></br><button id="returnButton" class="returnButton">Return</button><br></br><button id="addTrivia" class="triviaButton" onclick="addTrivia()">Add Trivia</button>';
+}
+
+function rentMovie() {}
+
+function returnMovie() {}
+
+function addTrivia() {
+  document.getElementById("triviaTextBox").innerHTML =
+    "<textarea id='triviaBox' rows='10' cols='50'></textarea><br><br><input type='submit' class='triviaButton' id='triviaSubmit' value='Send Trivia' onclick='postTrivia()'></input>";
+}
+function postTrivia() {
+  var newTrivia = document.getElementById("triviaBox").value;
+  var movieId = parseInt(localStorage.getItem("MovieId"));
+  if (newTrivia != null) {
+    var data = { filmid: movieId, trivia: newTrivia };
+
+    fetch("https://localhost:5001/api/FilmTrivia", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+  document.getElementById("triviaTextBox").innerHTML = "";
 }
 
 fetch("https://localhost:5001/api/Film")
